13d34529e5ed71d662d9a151cbcee05a05f84bb9
Marmacanna <brad.mathis64@gmail.com>
2020-04-25 15:59:06 -0700

    initial commit

---
+++ data.js
@@ -0,0 +1,38 @@
+const main = {
+    name: 'Bradley Mathis',
+    mail: 'brad.mathis64@gmail.com',
+    img: 'user.png',
+    role: ['Software Developer'],
+    // Refer https://ionicons.com/ and put the icon name inside iconName key
+    connects: [
+      {
+        name: 'Home',
+        iconName: 'logo-firefox',
+        link: ''
+      },
+      {
+        name: 'Github',
+        iconName: 'logo-github',
+        link: 'https://github.com/Marmacanna'
+      },
+      {
+        name: 'Linkedin',
+        iconName: 'logo-linkedin',
+        link: 'https://www.linkedin.com/in/brad-mathis-developer/'
+      },
+      {
+        name: 'StackOverflow',
+        iconName: 'logo-stackoverflow',
+        link: 'https://stackoverflow.com/users/8414175/marmacanna'
+      },
+      {
+        name: 'npm',
+        iconName: 'logo-npm',
+        link: ''
+      }
+    ],
+    links: [
+      { name: 'Projects', link: '#projects' },
+      { name: 'Blogs', link: '#blogs' }
+    ]
+  };
